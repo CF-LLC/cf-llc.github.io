ba6216d52a2542b58323c8dd538ea3b7999dcbdb
coopfeathy <cooperfeatherstone13@gmail.com>
2024-10-20 14:10:10 -0500

    again

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    assetPrefix: '/',
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/cooperfeatherstonellc',
+  assetPrefix: '/cooperfeatherstonellc/',
+}
+
+module.exports = nextConfig
